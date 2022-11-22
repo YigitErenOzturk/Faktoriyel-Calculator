f7b75b2f1e1d13e6a739bd8f0a85b39751e7d965
oztur <oztur@YIIT>
2022-11-22 14:02:46 +0100

    Proje dosyası ekle.

---
+++ Faktoriyel Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter A Number");
+int a = Convert.ToInt32(Console.ReadLine());
+int faktoriyel = 1;
+for (int i = 1; i <= a; i++)
+{
+    faktoriyel *= i;
+}
+Console.WriteLine(a + "! = " + faktoriyel);
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
